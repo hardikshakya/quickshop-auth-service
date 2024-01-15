89a03fe42d8d8e31d57c8f7c17c7c99cc7063e6b
hardikshakya <hardikshakya1997@gmail.com>
2024-01-15 09:43:26 +0530

    feat(auth): added JWT strategy along with some service functions

---
+++ src/auth/service/jwt.service.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@nestjs/common';
+import { JwtService as Jwt } from '@nestjs/jwt';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import * as bcrypt from 'bcryptjs';
+
+import { Auth } from '../auth.entity';
+
+@Injectable()
+export class JwtService {
+  @InjectRepository(Auth)
+  private readonly repository: Repository<Auth>;
+
+  private readonly jwt: Jwt;
+
+  constructor(jwt: Jwt) {
+    this.jwt = jwt;
+  }
+
+  // Decodes the provided JWT token
+  public async decode(token: string): Promise<unknown> {
+    return this.jwt.decode(token, null);
+  }
+
+  // Validates the user using the decoded information
+  public async validateUser(decoded: any): Promise<Auth> {
+    return this.repository.findOne(decoded.id);
+  }
+
+  // Generates a JWT token for the provided user
+  public generateToken(auth: Auth): string {
+    return this.jwt.sign({ id: auth.id, email: auth.email });
+  }
+
+  // Checks if the provided password matches the user's password
+  public isPasswordValid(password: string, userPassword: string): boolean {
+    return bcrypt.compareSync(password, userPassword);
+  }
+
+  // Encodes the provided password
+  public encodePassword(password: string): string {
+    const salt: string = bcrypt.genSaltSync(10);
+
+    return bcrypt.hashSync(password, salt);
+  }
+
+  // Verifies the provided JWT token, throws an error if the token is invalid
+  public async verify(token: string): Promise<any> {
+    try {
+      return this.jwt.verify(token);
+    } catch (err) {}
+  }
+}
---
+++ src/auth/strategy/jwt.strategy.ts
@@ -0,0 +1,24 @@
+import { Injectable, Inject } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+import { Auth } from '../auth.entity';
+import { JwtService } from '../service/jwt.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  @Inject(JwtService)
+  private readonly jwtService: JwtService;
+
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: process.env.JWT_SECRET,
+      ignoreExpiration: true,
+    });
+  }
+
+  private validate(token: string): Promise<Auth> {
+    return this.jwtService.validateUser(token);
+  }
+}
