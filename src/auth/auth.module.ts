e800241d651eba0ea0d2026f03cb8b56ef2d3184
hardikshakya <hardikshakya1997@gmail.com>
2024-01-15 09:46:34 +0530

    feat(auth): added controller and service functions

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,22 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AuthController } from './auth.controller';
+import { Auth } from './auth.entity';
+import { AuthService } from './service/auth.service';
+import { JwtService } from './service/jwt.service';
+import { JwtStrategy } from './strategy/jwt.strategy';
 
 @Module({
+  imports: [
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '365d' },
+    }),
+    TypeOrmModule.forFeature([Auth]),
+  ],
   controllers: [AuthController],
+  providers: [AuthService, JwtService, JwtStrategy],
 })
 export class AuthModule {}
